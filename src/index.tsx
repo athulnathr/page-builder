3e4e32fb18c63ba433a90a95740846bae20493ad
AthulNath <id4athul@gmail.com>
2022-07-14 18:27:34 +0530

    Resolve render warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
 import "./assets/index.scss";
 import Pages from './Pages';
+import ReactDOM from "react-dom/client";
 
 const Application: React.FC<{}>  = () => (
     <Pages/>
 )
-
-render(<Application />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<Application />);
